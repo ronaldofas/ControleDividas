17b01c2e0d66da57cbbd9c14ebfbf849cb95230c
Ronaldo Silva <ronaldofas@gmail.com>
2022-01-03 12:59:10 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Entities/Email.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Email
+    {
+        public int Id { get; set; }
+        public string EnderecoEmail { get; set; }
+
+        public Cliente cliente { get; set; }
+
+        protected Email() { }
+
+
+    }
+}
